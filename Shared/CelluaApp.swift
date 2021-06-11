49a2b4c1f0778af9c8783e340019bd6216086186
Markus Moenig <markus@moenig.io>
2021-06-11 07:28:34 +0700

    Initial Commit

---
+++ Shared/CelluaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelluaApp.swift
+//  Shared
+//
+//  Created by Markus Moenig on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CelluaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
